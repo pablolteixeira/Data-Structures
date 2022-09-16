e27986e0e966c14f4e31e52cf6ee69d6391fc5fe
Pablo Lopes <96208410+pablolteixeira@users.noreply.github.com>
2022-09-16 16:51:41 -0300

    Update stack.cc

--- stack.cc
+++ stack.cc
@@ -11,27 +11,16 @@ template<typename T>
 //! CLASSE PILHA
 class ArrayStack {
  public:
-    //! construtor simples
     ArrayStack();
-    //! construtor com parametro tamanho
     explicit ArrayStack(std::size_t max);
-    //! destrutor
     ~ArrayStack();
-    //! metodo empilha
     void push(const T& data);
-    //! metodo desempilha
     T pop();
-    //! metodo retorna o topo
     T& top();
-    //! metodo limpa pilha
     void clear();
-    //! metodo retorna tamanho
     std::size_t size();
-    //! metodo retorna capacidade maxima
     std::size_t max_size();
-    //! verifica se esta vazia
     bool empty();
-    //! verifica se esta cheia
     bool full();
 
  private:
